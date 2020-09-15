72b038d0f9b6b423973c38c24d96b47555d55a16
Mathieu Lièvre <mathieu.lievre@efrei.net>
2020-09-15 14:48:20 +0200

    ex3 correction

--- prelude.01/ex03.js
+++ prelude.01/ex03.js
@@ -1,14 +1,12 @@
-function Lenght(str : string = '') {
-    Let i = 0
 
+function length(string = '') {
+    let i = 0
+  
     for (let i in string)
-
-        i++
-    }
-
+      x++
+  
     return i
-
-}
-
-const size = Lenght(str. 'chopper')
-console.log(size)
+  }
+  
+  length('ch0pper')
+  
