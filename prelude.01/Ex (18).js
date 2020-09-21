406ca0fdb9f7a2804f21c5f8b1e67ef369ee1f23
Mathieu Lièvre <mathieu.lievre@efrei.net>
2020-09-21 18:06:42 +0200

    Added all 42 exercies js files

--- prelude.01/Ex (18).js
+++
@@ -1,7 +0,0 @@
-swapcase = function swapcase(str) {
-    return str.replace(/([a-z]+)|([A-Z]+)/g, function(match, chr) {
-        return chr ? match.toUpperCase() : match.toLowerCase();
-    });
-}
-console.log(swapcase('One Code'));
-console.log(swapcase('1337 Fruits'));
---
+++ prelude.01/ex03.js
@@ -0,0 +1,12 @@
+
+function length(string = '') {
+    let i = 0
+  
+    for (let i in string)
+      x++
+  
+    return i
+  }
+  
+  length('ch0pper')
+  
