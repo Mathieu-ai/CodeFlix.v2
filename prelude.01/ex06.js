f16a2435119e3f4a4a2f2571ed36b04f27c73bb0
Mathieu-ai <70896773+Mathieu-ai@users.noreply.github.com>
2020-09-21 20:19:47 +0200

    Update ex06.js

--- prelude.01/ex06.js
+++ prelude.01/ex06.js
@@ -1,7 +1,31 @@
-function spinalCase(str) {
-    return str.replace(/^[\W_]+|[\W_]+$|([\W_]+)/g, function ($0, $1) {
-                return $1 ? "-" : "";
-           }).replace(/([a-z])(?=[A-Z])/g, '$1-').toLowerCase();
+function lowerCase(str = '') {
+    const result = []
+    var oldCharCode
+
+    for (let i = 0; i < str.length; i++) {
+        let charCode = str.charCodeAt(i)
+        if (charCode == 32) {    // Si caractère Espace
+          //console.log(charCode + ' '+oldCharCode)
+          if (oldCharCode == 45)
+            continue;  
+          charCode = 45       // Remplace 32 par 45, remplace espace par tiret
+        } else if (charCode >= 65 && charCode <= 90) {  // est-ce une valeur minuscule ?
+          charCode = charCode + 32
+        }
+
+        result.push(String.fromCharCode(charCode));
+        oldCharCode = charCode
+    } 
+    return result.join('')
   }
   
-  console.log(spinalCase("love One code")); // "Love One code".
+
+  // TESTS
+  console.log(lowerCase('Chopper -Tony Tony'), 'RESULT: chopper')
+
+/*
+
+Avc utilisation de la librarie JS
+"     AAA   ffff BB     lkdfjgkj  ".trim().replace( /\s+/g, "-").toLowerCase()
+
+*/
