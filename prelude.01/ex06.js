b558cfd169080bce72fcd10036806c5e4e0e463d
Mathieu Lièvre <mathieu.lievre@efrei.net>
2020-09-22 07:50:35 +0200

    Syncro files

--- prelude.01/ex06.js
+++ prelude.01/ex06.js
@@ -1,7 +1,29 @@
-function spinalCase(str) {
-    return str.replace(/^[\W_]+|[\W_]+$|([\W_]+)/g, function ($0, $1) {
-                return $1 ? "-" : "";
-           }).replace(/([a-z])(?=[A-Z])/g, '$1-').toLowerCase();
+function spinalCase(str = '') {
+    const result = []
+    var oldCharCode              // contiendra le dernière caractère lu
+
+    for (let i = 0; i < str.length; i++) {
+        let charCode = str.charCodeAt(i)
+        if (charCode == 32) {    // Si caractère Espace
+          //console.log(charCode + ' '+oldCharCode)
+          if (oldCharCode == 45)
+            continue;  
+          charCode = 45       // Remplace 32 par 45, remplace espace par tiret
+        } else if (charCode >= 65 && charCode <= 90) {  // est-ce une valeur minuscule ?
+          charCode = charCode + 32
+        }
+
+        result.push(String.fromCharCode(charCode));     // empile le caractère transformé ou non
+        oldCharCode = charCode
+    } 
+    return result.join('')
   }
   
-  console.log(spinalCase("love One code")); // "Love One code".
+
+  // TESTS
+  console.log(spinalCase('Chopper -Tony Tony'), 'RESULT: chopper')
+
+/*
+Avc utilisation de la librarie JS
+"-----  AAA   ffff BB --------    lkdfjgkj  ".replace( /-+/g, " ").trim().replace( /\s+/g, "-").toLowerCase()
+*/
